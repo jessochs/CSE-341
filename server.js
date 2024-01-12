d9474b7253e0971f8e6f143750d2d7b218801af5
jessochs <fac17002@byui.edu>
2024-01-12 14:28:07 -0700

    routes folder and index file added

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
-const lesson1Controller = require('./controllers/lesson1')
- 
-app.get('/', lesson1Controller.loganRoute);
+
 
 const port = 3000;
- 
+
+app.use('/', require('./routes'));
+
 app.listen(process.env.PORT || port, () => {
   console.log('Web Server is listening at port ' + (process.env.PORT || 3000));
 });
